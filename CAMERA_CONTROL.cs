e2092c76e5f51da22becb8400726a278c8ab6c85
PilotPhil <632913013@qq.com>
2020-09-20 10:53:31 +0800

    add

---
+++ CAMERA_CONTROL.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAMERA_CONTROL : MonoBehaviour
+{
+    public Transform boat;// 传入要被跟随的船体
+    public GameObject FPP_LOOKAT_OBJ;
+
+    private int P_FLAG = 1;//视角切换
+
+    private void Update()
+    {
+        // 根据标志位切换视角
+        switch (P_FLAG)
+        {
+            case 1:
+                TPP();
+                break;
+
+            case 2:
+                FPP();
+                break;
+
+            case 3:
+                SKYP();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // 第三人称视角，船尾视角
+    private void TPP()
+    {
+        // 0.如果跟随的目标不存在，就直接返回
+        if (this.boat == null)
+            return;
+
+        // 1.摄像机移动到当前的物体所在的位置
+        this.transform.position = this.boat.position;
+
+        // 2.摄像机方向对船尾往后拉4米
+        //欧拉角转四元素，旧的Quaternion.EulerAngles已经废弃，旧的表示的是弧度，不是角度
+        Quaternion rot = Quaternion.Euler(0, this.boat.eulerAngles.y, 0);
+        //target的四元素旋转矩阵*摄像机自己的正Z方向=target的方向，target的方向*4=一个三维坐标向量
+        this.transform.position -= rot * Vector3.forward * 6;
+
+        // (3)将摄像机抬高一米
+        Vector3 position = this.transform.position;
+        position.y += 2;
+        this.transform.position = position;
+
+        // (4) 摄像机对准某个物体，将自己的Z轴对准目标
+        this.transform.LookAt(this.boat);
+    }
+
+    // 第一人称视角---未完工
+    private void FPP()
+    {
+        // 0.如果跟随的目标不存在，就直接返回
+        if (this.boat == null)
+            return;
+
+        // 1.摄像机移动到当前的物体所在的位置
+        this.transform.position = this.boat.position;
+
+        // 2.摄像机方向对船尾往前拉？米
+        //欧拉角转四元素，旧的Quaternion.EulerAngles已经废弃，旧的表示的是弧度，不是角度
+        Quaternion rot = Quaternion.Euler(0, this.boat.eulerAngles.y, 0);
+        //target的四元素旋转矩阵*摄像机自己的正Z方向=target的方向，target的方向*4=一个三维坐标向量
+        this.transform.position += rot * Vector3.forward * 2;
+
+        // (3)将摄像机抬高一米
+        Vector3 position = this.transform.position;
+        position.y += 1;
+        this.transform.position = position;
+
+        // (4) 摄像机对准某个物体，将自己的Z轴对准目标
+        this.transform.LookAt(this.FPP_LOOKAT_OBJ.transform);// BOAT根下添加一空物体，让摄像机指向位于船前面的空物体即可
+    }
+
+    // 天空视角，俯视图
+    private void SKYP()
+    {
+        // 0.如果跟随的目标不存在，就直接返回
+        if (this.boat == null)
+            return;
+
+        // 1.摄像机移动到当前的物体所在的位置
+        this.transform.position = this.boat.position;
+
+        // 2.摄像机方向对船尾往前拉？米
+        //欧拉角转四元素，旧的Quaternion.EulerAngles已经废弃，旧的表示的是弧度，不是角度
+        Quaternion rot = Quaternion.Euler(0, this.boat.eulerAngles.y, 0);
+        //target的四元素旋转矩阵*摄像机自己的正Z方向=target的方向，target的方向*4=一个三维坐标向量
+        this.transform.position += rot * Vector3.forward * 1;
+
+        // (3)将摄像机抬高11米
+        Vector3 position = this.transform.position;
+        position.y += 11;
+        this.transform.position = position;
+
+        // (4) 摄像机对准某个物体，将自己的Z轴对准目标
+        this.transform.LookAt(this.boat);
+    }
+
+    // 视角标志位设置
+    private void OnGUI()
+    {
+        if(GUILayout.Button("第三人称视角"))
+        {
+            P_FLAG = 1;
+        }
+        else if(GUILayout.Button("第一人称视角"))
+        {
+            P_FLAG = 2;
+        }
+        else if (GUILayout.Button("俯视视角"))
+        {
+            P_FLAG = 3;
+        }
+
+    }
+
+}
---
+++ SHIP_CONTROL.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SHIP_CONTROL : MonoBehaviour
+{
+    // 船体运动控制
+
+
+    private Rigidbody boat;// 船体刚体
+
+    [Range(0,10000)]
+    public int surge_force = 1000; // surge上力
+
+    [Range(0, 10000)]
+    public int yaw_monment = 1000;// yaw上力矩
+
+    void Start()
+    {
+        boat = GetComponent<Rigidbody>();// 获取刚体,要给主船体（船体外壳）加上mesh collider,并勾选convex,才能实现碰撞效果
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DOF2CONTROL();
+
+    }
+
+    private void DOF2CONTROL()
+    {
+        // 2DOF船舶控制
+        // 1.控制surge方向推进---添加一个相对于刚体的系统坐标力---AddRelativeForce
+        // 2.控制yaw航向---施加添加相对于刚体自身的坐标系统的一个力矩---AddRelativeTorque
+
+        // 问题（待解决）：
+        // 力应施加在螺旋桨上，其带动整条船运动
+        // 力矩应作用在舵上
+
+        //W、A、S、D控制
+        if (Input.GetKey("w"))// 按W在surge正向施加力
+        {
+            boat.AddRelativeForce(0, 0, surge_force * Time.deltaTime);
+        }
+        else if (Input.GetKey("s"))// 按S在surge负向施加力
+        {
+            boat.AddRelativeForce(0, 0, -1 * surge_force * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))// 按A在yaw逆时针施加力矩
+        {
+            boat.AddRelativeTorque(0, -1 * yaw_monment * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey("d"))// 按D在yaw顺时针施加力矩
+        {
+            boat.AddRelativeTorque(0, yaw_monment * Time.deltaTime, 0);
+        }
+    }
+}
